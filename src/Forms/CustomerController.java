87b3a97e117ce2ea948e344c0e476891b7f17c32
Oakley Dye <oakley@anasibusiness.com>
2020-10-19 08:00:17 -0600

    adding methods to handle updating, creating, and deleting of customer records

---
+++ src/Forms/CustomerController.java
@@ -0,0 +1,14 @@
+package Forms;
+
+import javafx.event.ActionEvent;
+
+public class CustomerController {
+    public void btnAdd_Click(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_Click(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_Click(ActionEvent actionEvent) {
+    }
+}
